7a0d2d4f1549d71b68fcff37b442de42d466fbbb
GitKsh <GitKsh>
2021-08-09 17:55:36 +0900

    1st

---
+++ src/main/java/com/sahkimkr/webservice/web/WebRestController.java
@@ -0,0 +1,28 @@
+package com.sahkimkr.webservice.web;
+
+import com.sahkimkr.webservice.domain.posts.PostsRepository;
+import com.sahkimkr.webservice.domain.posts.PostsSaveRequestDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
+    }
+}
