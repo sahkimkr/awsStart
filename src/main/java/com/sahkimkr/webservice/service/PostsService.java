6a66daa79b61674b98cb416e2f5ee6d4877a4a3f
GitKsh <GitKsh>
2021-08-11 18:02:09 +0900

    3rd

--- src/main/java/com/sahkimkr/webservice/service/PostsService.java
+++ src/main/java/com/sahkimkr/webservice/service/PostsService.java
@@ -1,10 +1,14 @@
 package com.sahkimkr.webservice.service;
 
+import com.sahkimkr.webservice.domain.posts.PostsMainResponseDto;
 import com.sahkimkr.webservice.domain.posts.PostsRepository;
 import com.sahkimkr.webservice.domain.posts.PostsSaveRequestDto;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 
@@ -16,5 +20,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/sahkimkr/webservice/web/WebController.java
+++ src/main/java/com/sahkimkr/webservice/web/WebController.java
@@ -1,6 +1,9 @@
 package com.sahkimkr.webservice.web;
 
+import com.sahkimkr.webservice.service.PostsService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.AllArgsConstructor;
@@ -8,8 +11,12 @@ import lombok.AllArgsConstructor;
 @Controller
 @AllArgsConstructor
 public class WebController {
+
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 }
